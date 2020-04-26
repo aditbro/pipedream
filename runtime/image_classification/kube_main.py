e0e4a4625ea7adc8f1f7a32e9e0c2b4cf95baeef
aditbro <adityafarizki@live.com>
2020-04-26 15:12:02 +0700

    redirect output to stderr

--- runtime/image_classification/kube_main.py
+++ runtime/image_classification/kube_main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 arg_list = os.getenv('arg_list')
 args_str = ''
@@ -7,7 +8,7 @@ for arg in arg_list.split(' ')[:-1]:
     arg_val = arg.replace('Q', '-')
     args_str += ' {} {} '.format(arg_val, os.getenv(arg))
 
-print('python main_with_runtime.py {} &> /home/aditya/ta_log/test'.format(args_str))
+sys.stderr.write('python main_with_runtime.py {}'.format(args_str))
 
-os.system('python main_with_runtime.py {}'.format(args_str))
+os.system('python main_with_runtime.py {} &> /home/aditya/ta_log/test'.format(args_str))
 
