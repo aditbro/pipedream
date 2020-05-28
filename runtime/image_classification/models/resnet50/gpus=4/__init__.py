8f43e58a169ba3adea474f38f237952b21131c16
aditbro <adityafarizki@live.com>
2020-05-29 05:33:14 +0700

    working version from server

--- runtime/image_classification/models/resnet50/gpus=4/__init__.py
+++ runtime/image_classification/models/resnet50/gpus=4/__init__.py
@@ -1,21 +1,16 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-from .resnet50 import ResNet50Split
+from .resnet50 import RESNET50Partitioned
 from .stage0 import Stage0
 from .stage1 import Stage1
-from .stage2 import Stage2
 
 def arch():
     return "resnet50"
 
 def model(criterion):
     return [
-        (Stage0(), ["input0"], ["out0", "out1"]),
-        (Stage1(), ["out0", "out1"], ["out3", "out2"]),
-        (Stage2(), ["out3", "out2"], ["out4"]),
-        (criterion, ["out4"], ["loss"])
+        (Stage0(), ["input0"], ["out1"]),
+        (Stage1(), ["out1"], ["out2"]),
+        (criterion, ["out2"], ["loss"])
     ]
 
 def full_model():
-    return ResNet50Split()
+    return RESNET50Partitioned()
